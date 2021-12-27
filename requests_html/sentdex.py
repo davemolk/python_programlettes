dd84bd011bc33ac9db2c26820e03cb22487168bb
davemolk <davemolk@gmail.com>
2021-12-27 10:46:47 -0700

    scraping

--- requests_html/sentdex.py
+++ requests_html/sentdex.py
@@ -18,77 +18,44 @@ intro = r.html.find('.introduction', first=True).text
 bs_link = r.html.find('.introduction a', first=True).links
 # print(bs_link)
 
+more_intro = r.html.find('.body p')[1].text
+# print(more_intro)
+
 
 # ****** languages at top ******
-
-
-'''
-# ****** languages at top ******
-
-description = soup.find('p', class_ = 'introduction').next_sibling.next_sibling.text
-
-languages = soup.find('div', class_ = 'body').ul
-# print(languages)
-# print(languages.contents) # gets all children, including new line characters
-# kiddos = []
-# for child in languages.children:
-#     kiddos.append(child)
-# print(kiddos) # this will be the same list as contents
-
-
-# for language in languages:
-#     print(language.string)
-# for language in languages.find_all('li'):
-#     print(language.text.strip())
+language_list = r.html.find('.body ul', first=True)
+# for language in language_list.find('li'):
+#     print(language.text)
 
 
 # ****** table ******
+table_rows = r.html.find('tr')
+# for td in table_rows[1:20]:
+#     print(td.text)
+    # range can go over and nothing bad happens
 
-# table = soup.find('table')
-# table_rows = table.find_all('tr')
-# my_languages = []
-# kittens = []
-# for tr in table_rows[1:8]:
-#     td = tr.find_all('td')
-#     row = [i.text for i in td]
-#     my_languages.append(row[0])
-#     kittens.append(row[2])
-# print(my_languages)
-# print(kittens)
+
+cats= []
+for tr in table_rows[1:8]:
+    td = tr.find('td')
+    kitten = td[2].text
+    cats.append(kitten)
+
+# print(cats)
 
 
 # ****** image ******
-image = soup.find('img', class_='responsive-img')['src']
+image = r.html.find('.responsive-img', first=True).attrs['src']
 # print(image)
 
 
 # ****** js ******
-js = soup.find('p', class_ = 'jstest').text
-# print(js)
-# need requests_html for dynamic data
+# r.html.render() ''' Need for JS to render '''
+
+jstest = r.html.find('.jstest', first=True).text
+# print(jstest)
 
 
-# ****** python credo *******
-koan = soup.find('pre').text
-# print(koan)
-
-
-# ****** footer ******
-end = soup.find('h5', class_ = 'white-text').text
-# print(end)
-
-contact = soup.find('div', class_ = 's12').p.text
-# print(contact)
-
-email = soup.find('p', string=re.compile('[\w-]+@[a-zA-Z]+\.[\w]+'))
-print(email)
-
-
-links = soup.find('div', class_ = 's12').ul
-link_list = []
-for link in links.find_all('li'):
-    link_list.append(link.a['href'])
-# print(link_list)
-
-
-'''
+# ****** zen of python *******
+zen = r.html.find('pre', first=True).text
+# print(zen)
