548af70fe304ea4e30ae6360021ba32395fc8e4b
davemolk <davemolk@gmail.com>
2022-01-03 06:14:00 -0700

    scraping

--- requests/python_jobs/r_html_scrape.py
+++ requests/python_jobs/r_html_scrape.py
@@ -13,7 +13,7 @@ class Scraper:
     def __init__(self):
         self.session = HTMLSession()
         self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'}
-        self.url = 'https://realpython.github.io/fake-jobs/2'
+        self.url = 'https://realpython.github.io/fake-jobs/'
 
     def get_data(self):
         try:
